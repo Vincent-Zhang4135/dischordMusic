cccb084b6c96e888221270991f18f913e42ef1e2
Vincent-Zhang4135 <65748334+Vincent-Zhang4135@users.noreply.github.com>
2021-10-07 21:10:15 -0500

    updating variable names

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require("discord.js");
+const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
 const ytdl = require("ytdl-core");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-// When the client is ready, run this code (only once)
-client.once("ready", () => {
-    console.log("Ready!");
-});
+const client = new Discord.Client()
 
 // Login to Discord with your client's token
 client.login(token);
+
+client.once("ready", () => {
+    console.log("Ready!");
+});
+client.once("reconnecting", () => {
+    console.log("Reconnecting!");
+});
+client.once("disconnect", () => {
+    console.log("Disconnect!");
+});
